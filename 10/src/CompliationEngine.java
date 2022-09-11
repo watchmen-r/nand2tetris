ad46fde94a9f775cef9af62d7026521bc3f79197
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-09-11 22:58:00 +0900

    add compile subroutineDec

--- 10/src/CompliationEngine.java
+++ 10/src/CompliationEngine.java
@@ -29,6 +29,9 @@ public class CompliationEngine {
 
         nextSymbol('{');
 
+        compileClassVarDec();
+        compileSubroutine();
+
     }
 
     public void compileClassVarDec() {
@@ -39,15 +42,16 @@ public class CompliationEngine {
             return;
         }
 
-        if (tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("constructor"))
-                || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("function"))
-                || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("method"))) {
+        if (!tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("constructor"))
+                || !tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("function"))
+                || !tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("method"))) {
             tokenizer.pointerBack();
             return;
         }
 
         // classVarDec starts 'static' or 'field'
-        if (!tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("static")) && tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("field"))) {
+        if (!tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("static"))
+                && !tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("field"))) {
             throw new Error("invalid source code.");
         }
 
@@ -58,7 +62,7 @@ public class CompliationEngine {
         // next is Type, according to book P233.
         compileType();
 
-        for(;;) {
+        for (;;) {
             tokenizer.advance();
             if (tokenizer.tokenType() != JackTokenizer.IDENTIFIER) {
                 throw new Error("invalid source code.");
@@ -83,17 +87,66 @@ public class CompliationEngine {
         compileClassVarDec();
     }
 
+    public void compileSubroutine() {
+        tokenizer.advance();
+
+        if (tokenizer.tokenType() == JackTokenizer.SYMBOL && tokenizer.symbol() == '}') {
+            tokenizer.pointerBack();
+            return;
+        }
+
+        // subroutineDec starts 'constructor' or 'functino' or 'method'
+        if (!tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("keyword"))
+                || (!tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("constructor"))
+                        || !tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("function"))
+                        || !tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("method")))) {
+            throw new Error("invalid source code.");
+        }
+
+        outputWriter.print("<subroutineDec>\n");
+        outputWriter.print("<keyword>" + tokenizer.getToken() + "</keyword>\n");
+        tokenPrintWriter.print("<keyword>" + tokenizer.getToken() + "</keyword>\n");
+
+        tokenizer.advance();
+        // check next token is void or type
+        if (tokenizer.tokenType().equals(JackTokenizer.KEYWORD) && tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("void"))) {
+            outputWriter.print("<keyword>void</keyword>\n");
+            tokenPrintWriter.print("<keyword>void</keyword>\n");
+        } else {
+            tokenizer.pointerBack();
+            compileType();
+        }
+
+        // next token has to be subroutineName which is identifier.
+        tokenizer.advance();
+        if (!tokenizer.tokenType().equals(JackTokenizer.IDENTIFIER)) {
+            throw new Error("invalid source code. Next token has to be identifier of subroutineName.");
+        }
+
+        outputWriter.print("<identifier>" + tokenizer.identifier() + "</identifier>\n");
+        tokenPrintWriter.print("<identifier>" + tokenizer.identifier() + "</identifier>\n");
+
+        nextSymbol('(');
+
+        // next token has to be parameterList
+        outputWriter.print("<parameterList>\n");
+        compileParameterList();
+        outputWriter.print("</parameterList>\n");
+
+        // TODO prior to implement compileParameterList
+    }
+
     public void compileType() {
         tokenizer.advance();
 
         // In case token is 'int' or 'char' or 'boolena'
-        if (tokenizer.tokenType() == JackTokenizer.KEYWORD 
-            && (tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("int"))
-            || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("char"))
-            || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("boolean")))) {
+        if (tokenizer.tokenType() == JackTokenizer.KEYWORD
+                && (tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("int"))
+                        || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("char"))
+                        || tokenizer.keyWord().equals(JackTokenizer.keyWordMap.get("boolean")))) {
             outputWriter.print("<keyword>" + tokenizer.getToken() + "</keyword>\n");
             tokenPrintWriter.print("<keyword>" + tokenizer.getToken() + "</keyword>\n");
-                
+
             // In case token is className(className is identifier)
         } else if (tokenizer.tokenType() == JackTokenizer.IDENTIFIER) {
             outputWriter.print("<keyword>" + tokenizer.getToken() + "</keyword>\n");
@@ -101,6 +154,12 @@ public class CompliationEngine {
         } else {
             throw new Error("invalid source code. Check type.");
         }
+    }
+    
+
+    // TODO implementation
+    public void compileParameterList() {
+
     }
 
     private void nextSymbol(char symbol) {
